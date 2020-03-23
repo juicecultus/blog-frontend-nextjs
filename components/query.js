5bd420f3ffb6a61c0f16ce293b634e389978ecae
juicecultus <juicecultus@gmail.com>
2020-03-23 00:26:14 +0000

    create Categories query and Nav component

--- components/query.js
+++ components/query.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
+// Using useQuery hook to call Strapi server at this address http://localhost:1337/graphql.
+// Sending an id if it exists (necessary when fetching just one article).
+
 const Query = ({ children, query, id }) => {
   const { data, loading, error } = useQuery(query, {
     variables: { id: id }
@@ -9,6 +12,7 @@ const Query = ({ children, query, id }) => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {JSON.stringify(error)}</p>;
 
+  // If request is successful, return the child component with the retrieved data as prop.
   return children({ data });
 };
 
